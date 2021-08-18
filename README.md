c70981f693245fcd1f8a28811fc6aa6299831ec3
Bright Amoateng <bright.amoateng150@gmail.com>
2021-08-18 18:59:04 -0400

    Creating my first two paths

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 An API created from the FastAPI python framework.\
 This was created from following a freeCodeCamp.org tutorial.
+
+## Running the app
+
+Install the necessary packages from pip.
+```
+pip install fastapi
+pip install uvicorn
+```
+
+Run the app.
+```
+uvicorn myapi:app
+```
+
+Access the api docs using `{URL_PATH}/docs`
+
+## Development
+
+Run the app with an additional parameter, to reload the web server on file changes
+```
+uvicorn myapi:app --reload
+```
---
+++ myapi.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+students = {
+    1: {
+        "name": "John",
+        "age": 17,
+        "class": "year 12"
+    },
+    2: {
+        "name": "Amanda",
+        "age": 15,
+        "class": "year 10"
+    },
+}
+
+@app.get("/")
+def index():
+    return {"name": "First Data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
+    return students[student_id]
