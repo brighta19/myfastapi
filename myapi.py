3d9359ffe66e92501d6c0b1da77819d71a31d852
Bright Amoateng <bright.amoateng150@gmail.com>
2021-08-18 21:55:13 -0400

    Use optional and required query parameters

--- myapi.py
+++ myapi.py
@@ -20,13 +20,14 @@ students = {
 def index():
     return {"name": "First Data"}
 
+# Use path parameters
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
     return students[student_id]
 
-# Use optional query parameters
+# Use optional and required query parameters
 @app.get("/get-by-name")
-def get_student(name: Optional[str] = None):
+def get_student(*, name: Optional[str] = None, test: int):
     for student_id in students:
         if students[student_id]["name"] == name:
             return students[student_id]
